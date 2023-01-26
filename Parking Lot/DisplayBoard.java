7c9dcd962f3b13317ee0581b806c375cf4d105de
Vaishali Ranjan <110801149+vaishaliranjan@users.noreply.github.com>
2023-01-26 23:13:54 +0530

    Code

---
+++ Parking Lot/DisplayBoard.java
@@ -0,0 +1,11 @@
+public class DisplayBoard {
+    public Status status;
+
+    public DisplayBoard() {
+        this.status = Status.AVAILABLE;
+    }
+
+    public void update(Status newStatus){
+        this.status = newStatus;
+    }
+}
---
+++ Parking Lot/Status.java
@@ -0,0 +1,3 @@
+public enum Status {
+    AVAILABLE, FULL;
+}
